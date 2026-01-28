f52aeb2565cfd8af894ca5e9a72d0d8db78c6b73
BMW-TH-LAPTOP\bluet <bluety1000@gmail.com>
2026-01-28 15:47:17 -0600

    the best code ever to exist in the entirity of man and human kind im not joking this is true

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LilBlueBot_PHC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LilBlueBot_PHC.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class LilBlueBot_PHC {
+    DcMotor LeftMotor;
+
+    {
+        LeftMotor = null;
+    }
+    DcMotor rightMotor = null;
 }
