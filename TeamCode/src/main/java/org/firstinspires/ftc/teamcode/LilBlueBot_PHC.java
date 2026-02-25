7bdd5be2edd8d5a06051ac506c13d02f69418933
BMW-TH-LAPTOP\bluet <bluety1000@gmail.com>
2026-02-25 16:16:09 -0600

    stuff was changed and yea yk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LilBlueBot_PHC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LilBlueBot_PHC.java
@@ -1,12 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LilBlueBot_PHC {
-    DcMotor LeftMotor;
+   public DcMotor LeftMotor;
 
     {
         LeftMotor = null;
     }
-    DcMotor rightMotor = null;
+   public DcMotor RightMotor;
+
+    {
+        RightMotor = null;
+    }
+
+    public void init(HardwareMap hwMap) {
+        LeftMotor = hwMap.dcMotor.get("leftMotor");
+        RightMotor = hwMap.dcMotor.get("rightMotor");
+
+    }
 }
