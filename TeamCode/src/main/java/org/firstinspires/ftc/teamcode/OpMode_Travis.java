2cbd357f347bf6ac928be57da91bc81fb441e15e
ASCENDEDSOUL8\kyleb <darkshadowz5622@gmail.com>
2026-03-18 15:46:42 -0500

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Travis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Travis.java
@@ -7,13 +7,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class OpMode_Travis extends OpMode {
 
     LilBlueBot_PHC blueBot = new LilBlueBot_PHC();
+    TBDGamepad gamepad;
 
     @Override
     public void init() {blueBot.init(hardwareMap);
+        gamepad = new TBDGamepad(gamepad1);
     }
 
     @Override
-    public void loop() {blueBot.leftMotor.setPower(-0.5);
-        blueBot.rightMotor.setPower(0.5);
+    public void loop() {
+        gamepad.update();
+
+        blueBot.tankDrive(gamepad.getLeftY(), gamepad.getRightY());
     }
 }
